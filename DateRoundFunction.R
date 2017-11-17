868158ac942edb683808db55366436ca1fd34411
DanielEBriggs <dbriggs@hsph.harvard.edu>
2017-11-17 14:46:17 -0500

    Function to source to round dates and times to the nearest 15 minutes

---
+++ DateRoundFunction.R
@@ -0,0 +1,20 @@
+myRound <- function (x, convert = TRUE)  {
+  as.Date(x)
+  x <- as.POSIXlt(x)
+  mins <- x$min
+  mult <- mins %/% 15
+  remain <- mins %% 15
+  if(remain > 7L || (remain == 7L && x$sec > 29))
+    mult <- mult + 1
+  if(mult > 3) {
+    x$min <- 0
+    x <- x + 3600
+  } else {
+    x$min <- 15 * mult
+  }
+  x <- trunc.POSIXt(x, units = "mins")
+  if(convert) {
+    x <- format(x, format = "%y-%m-%d %H:%M")
+  }
+  x
+}

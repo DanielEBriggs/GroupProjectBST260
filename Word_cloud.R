496c010e2c6dae16f0b95f84b840c638379592cd
Colby Wilkinson <cwilkinson@hsph.harvard.edu>
2017-11-19 15:40:56 -0500

    CleanTweets.R

---
+++ Word_cloud.R
@@ -0,0 +1,27 @@
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+cloud <- tokens %>%
+  select(word) %>%
+  mutate(word_1 = word) %>%
+  group_by(word) %>% 
+  mutate(n = n()) %>%
+  distinct(word, n)
+
+cloud %>%
+  distinct(word, n) %>%
+  filter(word == "nazi") %>%
+  select(n)
+  
+  
+wordcloud(words = cloud$word, freq = cloud$n, min.freq = 1,
+            max.words=200, random.order=FALSE, rot.per=0.35, 
+            colors=brewer.pal(8, "Dark2"))

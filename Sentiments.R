2f091a68dc627281d69219f91ac6ff42dc245853
Colby Wilkinson <cwilkinson@hsph.harvard.edu>
2017-11-28 21:57:30 -0500

    added day and time (in minutes) to tweet sentiment document

--- Sentiments.R
+++ Sentiments.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(tidytext)
 library(tidyverse)
+library(stringr)
+library(chron)
+
 data("stop_words")
 
 tweets <- read.csv("text_identities.csv", stringsAsFactors = FALSE)
@@ -30,5 +33,24 @@ tokens_afinn_sentiment <- tokens %>%
   inner_join(afinn_sentiments) %>%
   group_by(id, created) %>%
   mutate(tweet_sentiment = mean(score)) 
+
+tweet_sentiment_time <- tokens_afinn_sentiment %>%
+  mutate(day = "", day = replace(day, str_sub(created, start = 9, end = 10) == "29", "Sunday"),
+         day = replace(day, str_sub(created, start = 9, end = 10) == "28", "Saturday"),
+         day = replace(day, str_sub(created, start = 9, end = 10) == "27", "Friday"),
+         minutes = 60 * 24 * as.numeric(times(str_sub(created, start = -8, end = -1)))) %>%
+  select(created, id, screenName, retweetCount, tweet_sentiment, day, minutes)
+  
+  
+tweet_sentiment_time %>%
+  ggplot() +
+  geom_point(aes(x = minutes, y = tweet_sentiment, col = day))
+
+
+tweet_sentiment_time %>%
+  group_by(day) %>%
+  summarise(mean(tweet_sentiment))
+
+
 
 
